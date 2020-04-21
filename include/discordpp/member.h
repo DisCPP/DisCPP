c7dc97204c9f0db87cd6100642858274982efb26
Aidan Culley <legoguy253@gmail.com>
2020-04-21 14:01:04 -0500

    added documentation for user.h & member.h

--- include/discordpp/member.h
+++ include/discordpp/member.h
@@ -24,17 +24,17 @@ namespace discord {
 		bool HasRole(discord::Role role);
 		bool HasPermission(discord::Permission perm);
 
-		discord::User user;
-		snowflake guild_id;
-		std::string nick;
-		std::vector<discord::Role> roles;
+		discord::User user; /**< Discord user object */
+		snowflake guild_id; /**< ID of the guild the current member is in */
+		std::string nick; /**< Nickname of current member if it has one */
+		std::vector<discord::Role> roles; /**< Roles the current member has */
 		std::string joined_at; // TODO: Convert to iso8601Time
 		std::string premium_since; // TODO: Convert to iso8601Time
-		bool deaf;
-		bool mute;
-		discord::Permissions permissions;
-		discord::Activity activity;
-		int hierarchy;
+		bool deaf; /**< Whether or not the current member is deafened */
+		bool mute; /**< Whether or not the current member is muted */
+		discord::Permissions permissions; /**< Guild permissions for the current member */
+		discord::Activity activity; /**< Activity for the current member */
+		int hierarchy; /**< Role hierarchy for the current member */
 	};
 }
 
--- include/discordpp/user.h
+++ include/discordpp/user.h
@@ -42,19 +42,19 @@ namespace discord {
 		std::vector<Connection> GetUserConnections();
 		std::string GetAvatarURL(ImageType imgType = ImageType::AUTO);
 		//snowflake id;
-		std::string username;
-		std::string discriminator;
-		std::string avatar;
-		bool bot;
-		bool system;
-		bool mfa_enabled;
-		std::string locale;
-		bool verified;
-		std::string email;
+		std::string username; /**< Username of the current user */
+		std::string discriminator; /**< Discriminator of the current user */
+		std::string avatar; /**< Hashed avatar of the current user */
+		bool bot; /**< Whether or not the current user is a bot */
+		bool system; /**< Whether or not the current user is part of discord's system (Clyde) */
+		bool mfa_enabled; /**< Whether or not the current user has MFA enabled (only applies for the current user logged in) */
+		std::string locale; /**< Locale of the current user (only applies for the current user logged in) */
+		bool verified; /**< Whether or not the current bot user is verified */
+		std::string email; /**< Email of the current user (only applies for the current user logged in) */
 		int flags;
-		discord::specials::NitroSubscription premium_type;
-		std::string created_at;
-		std::string mention;
+		discord::specials::NitroSubscription premium_type; /**< The status of discord nitro for the current user */
+		std::string created_at; /**< The creation date of the current user's account */
+		std::string mention; /**< The @ mention of the current user Ex: <@150312037426135041> */
 	};
 }
 
