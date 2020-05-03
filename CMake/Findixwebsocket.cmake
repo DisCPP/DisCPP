fb204491ab6f9f76e6eead3f8720546f5860bb6b
SeanOMik <seanomik@gmail.com>
2020-05-03 17:10:02 -0500

    Fix some linker errors that some users may of ran into.

--- CMake/Findixwebsocket.cmake
+++ CMake/Findixwebsocket.cmake
@@ -3,7 +3,7 @@ find_path(IXWEBSOCKET_HEADERS ixwebsocket/IXWebSocket.h)
 find_library(IXWEBSOCKET_LIBRARY ixwebsocket)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(IXWEBSOCKET DEFAULT_MSG
+find_package_handle_standard_args(ixwebsocket DEFAULT_MSG
         IXWEBSOCKET_HEADERS IXWEBSOCKET_LIBRARY)
 
 mark_as_advanced(IXWEBSOCKET_HEADERS IXWEBSOCKET_LIBRARY)
