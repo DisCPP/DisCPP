75637f9cdf4c0726be791db08f58d918b79b9757
intexisty <legoguy253@gmail.com>
2020-05-16 23:53:11 -0500

    fix server info example

--- examples/serverinfo/CommandServerinfo.cpp
+++ examples/serverinfo/CommandServerinfo.cpp
@@ -14,17 +14,17 @@ bool ServerinfoCommand::CanRun(discpp::Context ctx) {
 }
 
 void ServerinfoCommand::CommandBody(discpp::Context ctx) {
-	discpp::Guild guild = ctx.message.guild;
+	std::shared_ptr<discpp::Guild> guild = ctx.message.guild;
 	discpp::EmbedBuilder* embed = new discpp::EmbedBuilder("", "", 0x3498DB);
-	embed->SetThumbnail(guild.GetIconURL());
-	std::string iconURL = guild.GetIconURL();
-	embed->AddField("Server Name:", guild.name, true);
-	embed->AddField("Server ID:", guild.id.c_str(), true);
-	embed->AddField("Created At:", guild.created_at, true);
-	std::string ownerId = guild.owner_id.c_str();
+	embed->SetThumbnail(guild->GetIconURL());
+	std::string iconURL = guild->GetIconURL();
+	embed->AddField("Server Name:", guild->name, true);
+	embed->AddField("Server ID:", std::to_string(guild->id), true);
+	embed->AddField("Created At:", guild->created_at, true);
+	std::string ownerId = std::to_string(guild->owner_id);
 	embed->AddField("Owner:", "<@!" + ownerId + ">", true);
-	embed->AddField("Members:", std::to_string(guild.member_count), true);
-	embed->AddField("Server Region:", guild.region, true);
-	embed->AddField("Verification Level:", std::to_string(guild.verification_level));
+	embed->AddField("Members:", std::to_string(guild->member_count), true);
+	embed->AddField("Server Region:", guild->region, true);
+	embed->AddField("Verification Level:", std::to_string(guild->verification_level));
 	ctx.channel.Send("", false, embed);
 }
