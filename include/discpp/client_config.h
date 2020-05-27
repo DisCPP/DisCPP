8eb37238568f9a61ddc97193a9b9cbe1e47e7d78
Intexisty <legoguy253@gmail.com>
2020-05-27 17:46:42 -0500

    updated clientconfig constructor to be const & byref

--- include/discpp/client_config.h
+++ include/discpp/client_config.h
@@ -3,6 +3,7 @@
 
 #include "log.h"
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace discpp {
@@ -18,8 +19,8 @@ namespace discpp {
 		int logger_flags;
 		int message_cache_size;
 		std::string logger_path;
-		ClientConfig(std::vector<std::string> prefixes, TokenType type = TokenType::BOT, int logger_flags = logger_flags::ERROR_SEVERITY | logger_flags::WARNING_SEVERITY, int message_cache_size = 5000, std::string logger_path = "")
-			: prefixes(prefixes), type(type), logger_flags(logger_flags), message_cache_size(message_cache_size), logger_path(logger_path) {}
+		ClientConfig(std::vector<std::string> prefixes, TokenType type = TokenType::BOT, int logger_flags = (unsigned int)logger_flags::ERROR_SEVERITY | (unsigned int)logger_flags::WARNING_SEVERITY, int message_cache_size = 5000, std::string logger_path = "")
+			: prefixes(std::move(prefixes)), type(type), logger_flags(logger_flags), message_cache_size(message_cache_size), logger_path(std::move(logger_path)) {}
 	};
 }
 #endif
