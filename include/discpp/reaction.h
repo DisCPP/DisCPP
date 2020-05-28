6baf76f171688b360be48d24b669820d553aa92c
Intexisty <legoguy253@gmail.com>
2020-05-28 13:48:50 -0500

    updated documentation for reaction

--- include/discpp/reaction.h
+++ include/discpp/reaction.h
@@ -11,6 +11,18 @@ namespace discpp {
 	class Reaction : public DiscordObject {
 	public:
 		Reaction() = default;
+
+        /**
+         * @brief Constructs a discpp::Reaction object by parsing json.
+         *
+         * ```cpp
+         *      discpp::Reaction reaction(json);
+         * ```
+         *
+         * @param[in] json The json that makes up the reaction object.
+         *
+         * @return discpp::Reaction, this is a constructor.
+         */
 		Reaction(rapidjson::Document& json);
 		Reaction(const int& count, const bool& from_bot, const discpp::Emoji& emoji) : count(count), from_bot(from_bot), emoji(emoji) { }
 
