382231ab069ace0093395b24f74f3ededa387a26
Intexisty <legoguy253@gmail.com>
2020-05-27 20:09:29 -0500

    updated documentation for context

--- include/discpp/context.h
+++ include/discpp/context.h
@@ -26,8 +26,39 @@ namespace discpp {
 		std::string remainder;
 		std::vector<std::string> arguments;
 
+        /**
+         * @brief Constructs a discpp::Context object.
+         *
+         * ```cpp
+         *      discpp::Context ctx(bot, channel, author, message, arguments);
+         * ```
+         *
+         * @param[in] bot The discpp bot.
+         * @param[in] channel The channel the command was ran in.
+         * @param[in] author The author of the message that executed the command.
+         * @param[in] message The message that was sent by the player
+         * @param[in] arguments The command arguments
+         *
+         * @return discpp::Context, this is a constructor.
+         */
 		Context(discpp::Client* client, discpp::Channel channel, std::shared_ptr<discpp::Member> author, const discpp::Message& message, std::string remainder, std::vector<std::string> arguments);
 
+        /**
+         * @brief Sends a text message (Shortcut for `channel.send(text, tts, embed, files)`).
+         *
+         * ```cpp
+         *      ctx.Send("Hello, I'm a bot!"); // Sending text
+         *      ctx.Send("", false, embed); // Sending an embed
+         *      ctx.Send("Command output was too large to fit in an embed.", false, nullptr, { file }); // Sending files
+         * ```
+         *
+         * @param[in] text The text that will be sent.
+         * @param[in] tts If the text could be spoken through text to speach.
+         * @param[in] embed Optional embed to send
+         * @param[in] files Option files to send
+         *
+         * @return discpp::Message
+         */
 		discpp::Message Send(const std::string& text, const bool& tts = false, discpp::EmbedBuilder* embed = nullptr, std::vector<discpp::File> files = {});
 	};
 }
--- src/context.cpp
+++ src/context.cpp
@@ -1,41 +1,8 @@
 #include "context.h"
 #include <utility>
 
-discpp::Context::Context(discpp::Client* client, discpp::Channel channel, std::shared_ptr<discpp::Member> author, const discpp::Message& message, std::string remainder, std::vector<std::string> arguments) : client(client), guild(message.guild), channel(std::move(channel)), author(std::move(author)), user(message.author), message(message), remainder(std::move(remainder)), arguments(std::move(arguments)) {
-	/**
-	 * @brief Constructs a discpp::Context object.
-	 *
-	 * ```cpp
-	 *      discpp::Context ctx(bot, channel, author, message, arguments);
-	 * ```
-	 *
-	 * @param[in] bot The discpp bot.
-	 * @param[in] channel The channel the command was ran in.
-	 * @param[in] author The author of the message that executed the command.
-	 * @param[in] message The message that was sent by the player
-	 * @param[in] arguments The command arguments
-	 *
-	 * @return discpp::Context, this is a constructor.
-	 */
-}
+discpp::Context::Context(discpp::Client* client, discpp::Channel channel, std::shared_ptr<discpp::Member> author, const discpp::Message& message, std::string remainder, std::vector<std::string> arguments) : client(client), guild(message.guild), channel(std::move(channel)), author(std::move(author)), user(message.author), message(message), remainder(std::move(remainder)), arguments(std::move(arguments)) {}
 
 discpp::Message discpp::Context::Send(const std::string& text, const bool& tts, discpp::EmbedBuilder* embed, std::vector<discpp::File> files) {
-	/**
-	 * @brief Sends a text message (Shortcut for `channel.send(text, tts, embed, files)`).
-	 *
-	 * ```cpp
-	 *      ctx.Send("Hello, I'm a bot!"); // Sending text
-	 *      ctx.Send("", false, embed); // Sending an embed
-	 *      ctx.Send("Command output was too large to fit in an embed.", false, nullptr, { file }); // Sending files
-	 * ```
-	 *
-	 * @param[in] text The text that will be sent.
-	 * @param[in] tts If the text could be spoken through text to speach.
-	 * @param[in] embed Optional embed to send
-	 * @param[in] files Option files to send
-	 *
-	 * @return discpp::Message
-	 */
-
 	return channel.Send(text, tts, embed, files);
 }
