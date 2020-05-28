0b84e94b52505da47b3d4c396b7147d336005308
Intexisty <legoguy253@gmail.com>
2020-05-28 13:16:04 -0500

    updated documentation for emoji

--- include/discpp/emoji.h
+++ include/discpp/emoji.h
@@ -21,6 +21,7 @@
 
 #include <locale>
 #include <string>
+#include <utility>
 
 namespace discpp {
 	class Guild;
@@ -28,10 +29,74 @@ namespace discpp {
 	class Emoji {
 	public:
 		Emoji() = default;
-		Emoji(const std::string& name, const snowflake& id);
-		Emoji(const discpp::Guild& guild, const snowflake& id);
-		Emoji(rapidjson::Document& json);
-		Emoji(const std::wstring& w_unicode);
+
+        /**
+         * @brief Constructs a discpp::Emoji object with a name and id.
+         *
+         * ```cpp
+         *      discpp::Emoji emoji("no_x", 657246994997444614);
+         * ```
+         *
+         * @param[in] name The name of the emoji
+         * @param[in] id The id of the emoji
+         *
+         * @return discpp::Emoji, this is a constructor.
+         */
+		Emoji(const std::string& name, const snowflake& id) : name(EscapeString(name)), id(id) {}
+
+        /**
+         * @brief Constructs a discpp::Emoji object using a guild object and id.
+         *
+         * This constructor searches the emoji cache in the guild object to get an emoji object.
+         *
+         * ```cpp
+         *      discpp::Emoji emoji(guild, 657246994997444614);
+         * ```
+         *
+         * @param[in] guild The guild that has this emoji.
+         * @param[in] id The id of the emoji.
+         *
+         * @return discpp::Emoji, this is a constructor.
+         */
+        Emoji(const discpp::Guild& guild, const snowflake& id);
+
+        /**
+         * @brief Constructs a discpp::Emoji object by parsing json.
+         *
+         * ```cpp
+         *      discpp::Emoji emoji(json);
+         * ```
+         *
+         * @param[in] json The json that makes up of emoji object.
+         *
+         * @return discpp::Emoji, this is a constructor.
+         */
+        Emoji(rapidjson::Document& json);
+
+        /**
+         * @brief Constructs a discpp::Emoji object with a std::wstring unicode representation.
+         *
+         * ```cpp
+         *      discpp::Emoji emoji( (std::wstring) L"\u0030");
+         * ```
+         *
+         * @param[in] w_unicode The std::wstring unicode representation of this emoji.
+         *
+         * @return discpp::Emoji, this is a constructor.
+         */
+		Emoji(std::wstring w_unicode) : unicode(std::move(w_unicode)) {}
+
+        /**
+         * @brief Constructs a discpp::Emoji object with a std::string unicode representation.
+         *
+         * ```cpp
+         *      discpp::Emoji emoji( (std::string) "\u0030");
+         * ```
+         *
+         * @param[in] s_unicode The std::string unicode representation of this emoji.
+         *
+         * @return discpp::Emoji, this is a constructor.
+         */
 		Emoji(const std::string& s_unicode);
 
         bool operator==(Emoji& other) const {
