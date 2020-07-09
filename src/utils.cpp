22cb9b6ef05a060baee7151ca1bc04986ee17f0c
SeanOMik <seanomik@gmail.com>
2020-07-08 19:05:04 -0500

    Remove the need for Vcpkg entirely.

--- src/utils.cpp
+++ src/utils.cpp
@@ -40,6 +40,10 @@ std::unique_ptr<rapidjson::Document> discpp::HandleResponse(cpr::Response& respo
         tmp->SetObject();
     }
 
+    if (response.error) {
+        globals::client_instance->logger->Error(LogTextColor::RED + "Hit error: " + response.error.message + " (Code: " + std::to_string((int)response.error.code) + ")");
+    }
+
     // Handle http response codes and throw an exception if it failed.
     if (response.status_code != 200 && response.status_code != 201 && response.status_code != 204) {
         std::string response_msg;
@@ -89,10 +93,10 @@ std::unique_ptr<rapidjson::Document> discpp::HandleResponse(cpr::Response& respo
 }
 
 std::string CprBodyToString(const cpr::Body& body) {
-	if (body.empty()) {
+	if (body.str().empty()) {
 		return "Empty";
 	}
-	
+
 	return body;
 }
 
@@ -103,10 +107,7 @@ std::unique_ptr<rapidjson::Document> discpp::SendGetRequest(const std::string& u
 	WaitForRateLimits(object, ratelimit_bucket);
 	cpr::Response result = cpr::Get(cpr::Url{ url }, headers, body);
 
-    std::unique_ptr<rapidjson::Document> doc = HandleResponse(result, object, ratelimit_bucket);
-
-    // This shows an error in inteliisense for some reason but compiles fine.
-	return doc;
+    return HandleResponse(result, object, ratelimit_bucket);
 }
 
 std::unique_ptr<rapidjson::Document> discpp::SendPostRequest(const std::string& url, const cpr::Header& headers, const Snowflake& object, const RateLimitBucketType& ratelimit_bucket, const cpr::Body& body) {
