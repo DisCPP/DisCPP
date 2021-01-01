da827a85092fd9ac56f0d7ce65e328f95e324873
SeanOMik <seanomik@gmail.com>
2020-12-31 18:38:29 -0600

    Fix typos in presence_update_event.h

---
+++ include/discpp/events/presence_update_event.h
@@ -0,0 +1,21 @@
+#ifndef DISCPP_PRESENCE_UDPATE_EVENT_H
+#define DISCPP_PRESENCE_UDPATE_EVENT_H
+
+#include "../event.h"
+#include "../user.h"
+#include "../presence.h"
+namespace discpp {
+	class PresenceUpdateEvent : public Event {
+	public:
+		inline PresenceUpdateEvent(Shard& shard, discpp::User user, discpp::Presence presence) : Event(shard), user(user), presence(presence) {}
+
+		discpp::User user;
+		discpp::Presence presence;
+
+        virtual int GetEventType() const override {
+            return 28;
+        }
+	};
+}
+
+#endif
