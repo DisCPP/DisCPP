edae2e8cb1d296ce026c8344563f68034d08afce
SeanOMik <seanomik@gmail.com>
2021-01-09 11:43:42 -0600

    Fix typo in NON_PRIVILEGED intent and also add a DEFAULT option.

--- include/discpp/client_config.h
+++ include/discpp/client_config.h
@@ -32,7 +32,7 @@ namespace discpp {
          *
          * @return ClientConfig, its a constructor.
          */
-		ClientConfig(std::vector<std::string> prefixes, uint64_t intents = discpp::GatewayIntents::NON_PRIVILAGED, TokenType type = TokenType::BOT, int shard_amount = 1, int logger_flags = (unsigned int)logger_flags::ERROR_SEVERITY | (unsigned int)logger_flags::WARNING_SEVERITY | (unsigned int)logger_flags::INFO_SEVERITY, int message_cache_size = 5000, std::string logger_path = "", int milli_sec_max_ratelimit = 60000)
+		ClientConfig(std::vector<std::string> prefixes, uint64_t intents = discpp::GatewayIntents::DEFAULT, TokenType type = TokenType::BOT, int shard_amount = 1, int logger_flags = (unsigned int)logger_flags::ERROR_SEVERITY | (unsigned int)logger_flags::WARNING_SEVERITY | (unsigned int)logger_flags::INFO_SEVERITY, int message_cache_size = 5000, std::string logger_path = "", int milli_sec_max_ratelimit = 60000)
 			: prefixes(std::move(prefixes)), intents(intents), type(type), shard_amount(shard_amount), logger_flags(logger_flags), message_cache_size(message_cache_size), logger_path(logger_path), milli_sec_max_ratelimit(milli_sec_max_ratelimit) {}
 	};
 }
